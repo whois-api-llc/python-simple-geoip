9ca857a24fab0acc2f795c9d2fc29f80a1b15327
Randall Degges <r@rdegges.com>
2018-04-23 23:08:41 -0700

    Adding an __init__.py for the module.

---
+++ simple_geoip/__init__.py
@@ -0,0 +1,16 @@
+"""
+simple_geoip
+~~~~~~~~~~~~
+
+An incredibly simple library that makes retrieving GeoIP data really easy. This
+library works by relying on the GeoIPify API service:
+https://geoipify.whoisxmlapi.com/
+"""
+
+
+__author__ = 'Randall Degges'
+__license__ = 'UNLICENSE'
+__version__ = '0.1.0'
+
+
+from .simple_geoip import GeoIP
